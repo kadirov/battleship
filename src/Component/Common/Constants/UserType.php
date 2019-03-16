5effaa7346201ceffc80ae2ac73118ceb047404a
Akmal Kadirov <akmal@kadirov.org>
2019-03-16 23:33:51 +0500

    add (UserType) constants

---
+++ src/Component/Common/Constants/UserType.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types=1);
+
+namespace App\Component\Common\Constants;
+
+/**
+ * Class UserType
+ * @package App\Component\Common\Constants
+ */
+class UserType
+{
+    /**
+     * User
+     */
+    public const USER = 10;
+
+    /**
+     * CPU
+     */
+    public const CPU = 20;
+}
